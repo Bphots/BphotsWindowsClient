f31e7dbbedf75af633907de537a4e322883f1979
JiYuhang <wakuflair@163.com>
2017-06-11 10:09:31 +0800

    删除各种浏览器

--- HotsBpHelper/Pages/BpViewModel.cs
+++ HotsBpHelper/Pages/BpViewModel.cs
@@ -13,7 +13,6 @@ namespace HotsBpHelper.Pages
             string filePath = Path.Combine(App.AppPath, Const.LOCAL_WEB_FILE_DIR, "index.html");
             LocalFileUri = new Uri(filePath, UriKind.Absolute).AbsoluteUri;
 //            Debug.WriteLine(LocalFileUri.IsWellFormedOriginalString());
-//            LocalFileUri = new Uri("http://www.163.com");
         }
     }
 }
