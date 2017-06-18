37d788dfd3864883b1f3c0b3d0d58dc22dc0a4fa
JiYuhang <wakuflair@163.com>
2017-06-18 08:18:39 +0800

    web api

--- HotsBpHelper/Utils/FileUtil.cs
+++ HotsBpHelper/Utils/FileUtil.cs
@@ -7,15 +7,5 @@ namespace HotsBpHelper.Utils
 {
     public class FileUtil
     {
-        public static string CheckMD5(string filename)
-        {
-            using (var md5 = MD5.Create())
-            {
-                using (var stream = File.OpenRead(filename))
-                {
-                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", String.Empty);
-                }
-            }
-        }
     }
 }
