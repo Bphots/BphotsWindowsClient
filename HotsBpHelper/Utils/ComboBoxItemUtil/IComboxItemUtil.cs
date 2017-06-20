c33ced9d0cd90070e05dfba360d75ca647144d5e
JiYuhang <wakuflair@163.com>
2017-06-20 23:08:00 +0800

    MapSelector

---
+++ HotsBpHelper/Utils/ComboBoxItemUtil/IComboxItemUtil.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HotsBpHelper.Utils.ComboBoxItemUtil
+{
+    public interface IComboxItemUtil
+    {
+        IEnumerable<ItemInfo> GetComboxItemInfos();
+    }
+}
