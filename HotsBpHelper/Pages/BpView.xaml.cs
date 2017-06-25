e0c177d2e8b17d9065de1d1f2ecd3c65fc6473fb
JiYuhang <wakuflair@163.com>
2017-06-25 22:22:16 +0800

    showwindow

--- HotsBpHelper/Pages/BpView.xaml.cs
+++ HotsBpHelper/Pages/BpView.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System.Windows;
+using HotsBpHelper.Messages;
 using HotsBpHelper.UserControls;
 using Stylet;
 
 namespace HotsBpHelper.Pages
 {
-    public partial class BpView : Window, IHandle<InvokeScriptMessage>
+    public partial class BpView : Window, IHandle<InvokeScriptMessage>, IHandle<ShowWindowMessage>
     {
         public BpView(IEventAggregator eventAggregator)
         {
@@ -15,6 +16,10 @@ namespace HotsBpHelper.Pages
         public void Handle(InvokeScriptMessage message)
         {
             Browser.InvokeScript(message);
+        }
+
+        public void Handle(ShowWindowMessage message)
+        {
         }
     }
 }
