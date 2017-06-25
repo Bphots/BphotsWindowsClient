531585ccb1520a8c147b53e5787776d2d7c4e50f
JiYuhang <wakuflair@163.com>
2017-06-25 19:35:39 +0800

    timestamp

--- HotsBpHelper/Api/Security/SecurityProvider.cs
+++ HotsBpHelper/Api/Security/SecurityProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using HotsBpHelper.Utils;
 using RestSharp;
@@ -8,16 +9,32 @@ namespace HotsBpHelper.Api.Security
 {
     public class SecurityProvider : ISecurityProvider
     {
+        private DateTime _startDateTime;
+
+        private double _timestamp;
+
+        public void SetServerTimestamp(double timestamp)
+        {
+            _startDateTime = DateTime.Now;
+            _timestamp = timestamp;
+        }
+
         public SecurityParameter CaculateSecurityParameter(IList<Tuple<string, string>> parameters)
         {
             var sp = new SecurityParameter();
             var strParams = parameters.OrderBy(tuple => tuple.Item1).Select(tuple => $"\"{tuple.Item1}\":\"{tuple.Item2}\"");
             string param = "{" + string.Join(",", strParams) + "}";
-            sp.Timestamp = ((int)DateTime.Now.ToUnixTimestamp()).ToString();
+            sp.Timestamp = CalcNewTimestamp();
             sp.Patch = Const.PATCH;
             sp.Nonce = Guid.NewGuid().ToString().Substring(0, 8);
             sp.Sign = Md5Util.CaculateStringMd5($"{Const.KEY}-{sp.Timestamp}-{sp.Patch}-{sp.Nonce}-{param}");
             return sp;
+        }
+
+        private string CalcNewTimestamp()
+        {
+            int delta = (int)(_timestamp + (DateTime.Now.ToUnixTimestamp() - _startDateTime.ToUnixTimestamp()));
+            return delta.ToString();
         }
     }
 }
