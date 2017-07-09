7839b6c27cf48ef7050995183473cbcc37b7fe94
monders <monders@qq.com>
2017-07-10 00:01:04 +0800

    增加分辨率 2560*1080与其他

---
+++ HotsBpHelper/Api/Security/ISecurityProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotsBpHelper.Api.Security
+{
+    public interface ISecurityProvider
+    {
+        void SetServerTimestamp(double timestamp);
+        SecurityParameter CaculateSecurityParameter(IList<Tuple<string, string>> parameters);
+    }
+}
