50d81d6638a568b5b5e69fd8740258455cf87aa5
JiYuhang <wakuflair@163.com>
2018-01-01 22:09:02 +0800

    call C# from javascript

---
+++ HotsBpHelper/UserControls/ScriptingHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HotsBpHelper.UserControls
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [ComVisible(true)]
+    public class ScriptingHelper
+    {
+        private readonly UserControl _userControl;
+
+        public ScriptingHelper(UserControl userControl)
+        {
+            _userControl = userControl;
+        }
+
+        public void HideWindow()
+        {
+            var win = Window.GetWindow(_userControl);
+            if (win != null)
+            {
+                win.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
