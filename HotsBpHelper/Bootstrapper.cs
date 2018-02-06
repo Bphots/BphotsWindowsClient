7525757858ab42cc3bcdb69aaf15d72f3c26168a
qqytqqyt <ytong007@hotmail.com>
2018-02-06 00:53:21 +0000

    [HotsBpHelper] 加入/devtool参数, 修复一个无法退出的bug

--- HotsBpHelper/Bootstrapper.cs
+++ HotsBpHelper/Bootstrapper.cs
@@ -63,6 +63,11 @@ namespace HotsBpHelper
                 OcrEngine.Debug = true;
             }
 
+            if (args.Any(arg => arg.ToLower() == "/devtool"))
+            {
+                App.DevTool = true;
+            }
+
             if (args.Any(arg => arg.ToLower() == "/debug"))
             {
                 LogUtil.NoLog = false;
