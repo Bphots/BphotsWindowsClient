28bfb2e47ba681933b8ca7328be2a862571c44a3
qqytqqyt <ytong007@hotmail.com>
2018-02-08 19:07:02 +0000

    [HotsBpHelper] 加入config.ini

--- HotsBpHelper/Configuration/ConfigureFileParser.cs
+++ HotsBpHelper/Configuration/ConfigureFileParser.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DotNetHelper;
 
-namespace HotsBpHelper.ConfigurationHelper
+namespace HotsBpHelper.Configuration
 {
     public class ConfigureFileParser
     {
