5382f2837fa4f1a6c4a121673aacd9b7608964b2
qqytqqyt <ytong007@hotmail.com>
2018-02-11 22:09:24 +0000

    [HotsBpHelper] 完善回传逻辑

--- HotsBpHelper/Pages/ManagerViewModel.cs
+++ HotsBpHelper/Pages/ManagerViewModel.cs
@@ -4,15 +4,28 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotsBpHelper.Uploader;
 using HotsBpHelper.UserControls;
 using Newtonsoft.Json;
 using Stylet;
 
 namespace HotsBpHelper.Pages
 {
+    public enum SettingsTab
+    {
+        Replay,
+        Settings,
+        BigData,
+        About
+    }
+
     public class ManagerViewModel : ViewModelBase
     {
+        public SettingsTab SettingsTab { get; set; }
+
         private readonly IEventAggregator _eventAggregator;
+
+        public HashSet<SettingsTab> PopulatedTabs = new HashSet<SettingsTab>();
         
         public ManagerViewModel(IEventAggregator eventAggregator)
         {
@@ -20,20 +33,71 @@ namespace HotsBpHelper.Pages
             var filePath = Path.Combine(App.AppPath, Const.LOCAL_WEB_FILE_DIR, "manager.html#") + App.Language;
             LocalFileUri = filePath;
         }
-
+        
         public void ShowSettings()
         {
+            SettingsTab = SettingsTab.Settings;
             _eventAggregator.PublishOnUIThread(new InvokeScriptMessage
             {
                 ScriptName = "setTab",
                 Args = new[] { "Settings" }
             }, "ManagerChannel");
 
+            if (!PopulatedTabs.Contains(SettingsTab.Settings))
+            {
+                PopulateSettings();
+            }
+        }
+
+        public void PopulateSettings()
+        {
             _eventAggregator.PublishOnUIThread(new InvokeScriptMessage
             {
                 ScriptName = "configure",
-                Args = new[] { JsonConvert.SerializeObject(App.CustomConfigurationSettings) }
+                Args = new[] {JsonConvert.SerializeObject(App.CustomConfigurationSettings)}
             }, "ManagerChannel");
+            PopulatedTabs.Add(SettingsTab.Settings);
+        }
+
+        public void ShowReplays(Manager uploadManager)
+        {
+            SettingsTab = SettingsTab.Replay;
+            _eventAggregator.PublishOnUIThread(new InvokeScriptMessage
+            {
+                ScriptName = "setTab",
+                Args = new[] { "Replays" }
+            }, "ManagerChannel");
+
+            if (!PopulatedTabs.Contains(SettingsTab.Replay))
+            {
+                PopulateUploadManager(uploadManager);
+            }
+        }
+
+        public void PopulateUploadManager(Manager uploadManager)
+        {
+            uploadManager.ReplayFileStatusChanged -= UpdateReplay;
+            _eventAggregator.PublishOnUIThread(new InvokeScriptMessage
+            {
+                ScriptName = "PopulateReplayFiles",
+                Args = new[] {JsonConvert.SerializeObject(uploadManager.Files.ToList())}
+            }, "ManagerChannel");
+            uploadManager.ReplayFileStatusChanged += UpdateReplay;
+            PopulatedTabs.Add(SettingsTab.Replay);
+        }
+
+        private void UpdateReplay(object sender, EventArgs<ReplayFile> e)
+        {
+            _eventAggregator.PublishOnUIThread(new InvokeScriptMessage
+            {
+                ScriptName = "UpdateReplayFile",
+                Args = new[] { JsonConvert.SerializeObject(e.Data) }
+            }, "ManagerChannel");
+        }
+
+        public void ResetEvenHandler(Manager uploadManager)
+        {
+            uploadManager.ReplayFileStatusChanged -= UpdateReplay;
         }
         
         public string LocalFileUri { get; set; }
---
+++ HotsBpHelper/UserControls/WebCallbackListener.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chromium.Remote.Event;
+using Chromium.WebBrowser;
+using HotsBpHelper.Configuration;
+using HotsBpHelper.Settings;
+using Newtonsoft.Json;
+
+namespace HotsBpHelper.UserControls
+{
+    public class WebCallbackListener : JSObject
+    {
+        public WebCallbackListener()
+        {
+            AddFunction("callback").Execute += Callback;
+        }
+
+        public event EventHandler<string> InfoRequested;
+
+        private void Callback(object sender, CfrV8HandlerExecuteEventArgs e)
+        {
+            var args = e.Arguments;
+            if (args.Length < 2 || !args[0].IsString)
+                return;
+
+            if (args[0].StringValue == "SaveConfig")
+            {
+                var newConfig = JsonConvert.DeserializeObject<CustomConfigurationSettings>(args[1].StringValue);
+                BpHelperConfigParser.WriteConfig(newConfig);
+                App.CustomConfigurationSettings = newConfig;
+            }
+
+            if (args[0].StringValue == "Request")
+            {
+                OnInfoRequested(args[0].StringValue);
+            }
+        }
+
+        protected virtual void OnInfoRequested(string e)
+        {
+            InfoRequested?.Invoke(this, e);
+        }
+    }
+}
