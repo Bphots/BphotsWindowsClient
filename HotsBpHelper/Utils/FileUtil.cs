c9ce74945035100349d7a83f99bbc8e740e18675
qqytqqyt <ytong007@hotmail.com>
2018-02-26 00:43:22 +0000

    [HotsBpHelper] 移除测试文件

--- HotsBpHelper/Utils/FileUtil.cs
+++ HotsBpHelper/Utils/FileUtil.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using DotNetHelper;
 
 namespace HotsBpHelper.Utils
 {
-    public class FileUtil
+    public static class FileUtil
     {
+        public static void CleanUpImageTestFiles()
+        {
+            DirectoryInfo di = new DirectoryInfo(@".\Images\Heroes\Test");
+            if (!di.Exists)
+                return;
+
+            foreach (var file in di.GetFiles().ToList())
+            {
+                try
+                {
+                    file.Delete();
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+        }
+
     }
 }
