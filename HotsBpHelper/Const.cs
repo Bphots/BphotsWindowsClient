996bd9ded7657891bec77b5db74a1e0122204361
qqytqqyt <ytong007@hotmail.com>
2018-02-27 23:45:21 +0000

    [HotsBpHelper] 版本更新

--- HotsBpHelper/Const.cs
+++ HotsBpHelper/Const.cs
@@ -11,7 +11,7 @@ namespace HotsBpHelper
 
         public const string LOCAL_WEB_FILE_DIR = "WebFiles";
         
-        public const string PATCH = "18022601";
+        public const string PATCH = "18022801";
 
         public const string UPDATE_FEED_XML = "https://www.bphots.com/bp_helper/get/update?patch=" + PATCH;
 
