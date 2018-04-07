f7895ae5487b44102f45785a858e8011f8d49961
qqytqqyt <ytong007@hotmail.com>
2018-04-07 22:07:08 +0100

    [HotsBpHelper] 加入删除工程

---
+++ ServiceUninstaller/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ServiceUninstaller
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Uninstalling...");
+                var process = Process.Start(new ProcessStartInfo()
+                {
+                    FileName = @"cmd.exe",
+                    UseShellExecute = true,
+                    Arguments = "/C sc stop \"HotsBpHelper - Monitor\"&sc delete \"HotsBpHelper - Monitor\"",
+                    Verb = "runas",
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                });
+
+                process.WaitForExit();
+                return 0;
+            }
+            catch (Exception)
+            {
+                return 1;
+            }
+        }
+    }
+}
